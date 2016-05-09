c8ed593b64d9ba80df145ac29cdc994c954f517b
madrid13 <madridmaxime13@gmail.com>
2016-05-09 12:24:24 +0200

    exercice1 php

---
+++ exo1.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <?php
+    $x = "5";
+    $y = "8";
+      ?>
+    <p> <?php echo $x + $y; ?> </p>
+    <p> <?php echo $x * $y; ?> </p>
+
+  </body>
+</html> 
